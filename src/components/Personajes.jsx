2587d9dedaf2bae1c6a1387b809237b7e9e18c82
Estefaniamarinma <emarin7984@misena.edu.co>
2021-06-21 18:34:21 -0500

    nuevos cambios

--- src/components/Personajes.jsx
+++ src/components/Personajes.jsx
@@ -6,71 +6,56 @@ import Text_personajes from "./text_personajes";
 
 const Personajes = () => {
   return (
-    <section className=" py-5">
-      <Text_personajes />
-      <div className="row justify-content-center container  ">
-        <article
-          className=" mt-5  border-0  p-3 mb-5 text-center  " 
-          
-        >
+    <section>
+        <Text_personajes/>
+        <div className="container py-5">
+          <div className="row justify-content-center ">
           <Cards
-            photo={Personaje_mujer}
-            title="Katherine Johnson"
-            text=" Matemática NASA"
-          />
-        </article>
-        <article
-          className=" mt-5  border-0  p-3 mb-5 text-center   "
-          
-        >
+       photo={Personaje_mujer}
+       title="Katherine Johnson"
+       text=" Matemática NASA"
+     />
+      <Cards
+       photo={Personaje_hombre}
+       title="Eugene Kranz"
+       text=" Director Vuelo/Equipo Blanco"
+     />
+     <Cards
+       photo={Personaje_hombre}
+       title="Serguéi Koroliov"
+       text=" Científico"
+     />
+
+          </div>
+          <div className="row justify-content-center ">
           <Cards
-            photo={Personaje_hombre}
-            title="Eugene Kranz"
-            text=" Director Vuelo/Equipo Blanco"
-          />
-        </article>
-        <article
-          className="mt-5  border-0  p-3 mb-5 text-center  "
-          
-        >
-          <Cards
-            photo={Personaje_hombre}
-            title="Serguéi Koroliov"
-            text=" Científico"
-          />
-        </article>
-        <article
-          className=" mt-5  border-0  p-3 mb-5 text-center   "
-          
-        >
-          <Cards
-            photo={Personaje_hombre}
-            title="Neil Armstrong"
-            text=" Astronauta"
-          />
-        </article>
-        <article
-          className=" mt-5  border-0  p-3 mb-5 text-center   "
-         
-        >
-          <Cards
-            photo={Personaje_hombre}
-            title="Buzz Aldrin"
-            text=" Ingeniero/Astronauta"
-          />
-        </article>
-        <article
-          className=" mt-5  border-0  p-3 mb-5 text-center   "
-         
-        >
-          <Cards
-            photo={Personaje_hombre}
-            title="Michael Collins"
-            text=" Astronauta"
-          />
-        </article>
-      </div>
+       photo={Personaje_hombre}
+       title="Neil Armstrong"
+       text=" Astronauta"
+     />
+      <Cards
+       photo={Personaje_hombre}
+       title="Buzz Aldrin"
+       text=" Ingeniero/Astronauta"
+     />
+     <Cards
+       photo={Personaje_hombre}
+       title="Michael Collins"
+       text=" Astronauta"
+     />
+
+          </div>
+          </div>
     </section>
+    
+           
+
+                    
+           
+
+      
+       
+       
   );
 };
 
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,9 +1,9 @@
 import "../styles/main-comic.css";
 const Cards = ({ photo, title, text }) => {
     return (
-      <article className="card-body text-center content-zoom  shadow-lg ">
+      <article className="card-body text-center content-zoom  shadow-lg  mb-7">
         <picture>
-          <img src={photo} className="img-personaje rounded-circle img-fluid " alt="Img-nav" />
+          <img src={photo} className="img-personaje rounded-circle img-fluid  " alt="Img-nav" />
         </picture>
         <div className="text mb-4">
           <h4>{title}</h4>
@@ -14,3 +14,4 @@ const Cards = ({ photo, title, text }) => {
     );
   };
   export default Cards;
+  
