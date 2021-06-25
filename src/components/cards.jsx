fb26bc2186dd86264eb20e4ccceb57b7910219d3
Estefaniamarinma <emarin7984@misena.edu.co>
2021-06-25 15:47:33 -0500

    no importa :v

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -2,10 +2,10 @@ import "../styles/main-comic.css";
 //mt-5 border-0  p-3 mb-5 
 const Cards = ({ photo, title, text }) => {
     return (
-<article className="mt-5  border-0 content-zoom shadow-lg p-3 mb-5 card-body text-center">
+<article className="mt-5 shadow-lg  content-zoom border-0  p-3 mb-5 card-body text-center">
 
         <picture>
-          <img src={photo} className="img-personaje rounded-circle img-fluid .cardstyle " alt="Img-personajes" />
+          <img src={photo} className="img-personaje    rounded-circle img-fluid .cardstyle " alt="Img-personajes" />
         </picture>
         <div className="card-body text-center">
           <h4 classname="subtitle">{title}</h4>
