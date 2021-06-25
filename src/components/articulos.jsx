780f17588b8408b26b1578dafe9d3ee4494a92d6
Juan Camilo Gutierrez <jcgutierrez1300@misena.edu.co>
2021-06-25 11:55:21 -0500

    shop changes

--- src/components/articulos.jsx
+++ src/components/articulos.jsx
@@ -10,101 +10,110 @@ import Gorra from '../images/gorra 111.png';
 const Articulos = () => {
 
     return (
-        <div className = "row">
 
-            <div className="col-lg-4 col-md-6 mb-4">
+        <div className="container">
 
-                <div className="card h-100">
-                    <a href="#"><img src={Chompa} alt="articulo1"/></a>
-                    <div className="card-body">
-                        <h4 className="card-title">
-                            <a className="article-tittle text-bold" href="#">Gradient T-shirt</a>
-                        </h4>
-                        <h5 className="subtitle">COP$ 60.000</h5>
-                        <p className="card-text parrafo">Hermosa camiseta de color degradado azul-morado</p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-4 col-md-6 mb-4">
-                <div className="card h-100">
-                    <a href="#"><img src={Chompa} alt="articulo2"/></a>
-                    <div className="card-body">
-                        <h4 className="card-title">
-                            <a className="article-tittle text-bold" href="#">Gradient Hoddie</a>
-                        </h4>
-                        <h5 className="subtitle">COP$ 120.000</h5>
-                        <p className="card-text parrafo">Increible hoddie con degradado de color morado-negro </p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-4 col-md-6 mb-4">
-                <div className="card h-100">
-                    <a href="#"><img src={BlackTshirt} alt="articulo3"/></a>
-                    <div className="card-body">
-                        <h4 className="card-title">
-                            <a className="article-tittle text-bold" href="#">T-shirt Nuevo Formato</a>
-                        </h4>
-                        <h5 className="subtitle">COP$ 40.000</h5>
-                        <p className="card-text parrafo">Camiseta negra nuevo formato</p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-4 col-md-6 mb-4">
-                <div className="card h-100">
-                    <a href="#"><img src={WhiteTshirt} alt="articulo4"/></a>
-                    <div className="card-body">
-                        <h4 className="card-title">
-                            <a className="article-tittle text-bold" href="#">T-shirt Nuevo Formato</a>
-                        </h4>
-                        <h5 className="subtitle">COP$ 40.000</h5>
-                        <p className="card-text parrafo">Camiseta blanca nuevo formato</p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-4 col-md-6 mb-4">
-                <div className="card h-100">
-                    <a href="#"><img src={Totebag} alt="articulo5"/></a>
-                    <div className="card-body">
-                        <h4 className="card-title">
-                            <a className="article-tittle text-bold" href="#">Tote Bag Logo Cientonce</a>
-                        </h4>
-                        <h5 className="subtitle">COP$ 20.000</h5>
-                        <p className="card-text parrafo">hermoso bolso ecológico para salir de compras y cuidar el medio ambiente</p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-4 col-md-6 mb-4">
-                <div className="card h-100">
-                    <a href="#"><img src={Gorra} alt="articulo6"/></a>
-                    <div className="card-body">
-                        <h4 className="card-title">
-                            <a className="article-tittle text-bold" href="#">Gorra Logo Cientonce</a>
-                        </h4>
-                        <h5 className="subtitle">COP$ 25.000</h5>
-                        <p className="card-text parrafo">gorra negra con costuras moradas tipo baceball</p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                </div>
-            </div>
+<div className = "row">
+
+<div className="col-lg-4 col-md-6 mb-4">
+
+    <div className="card ">
+        <a href="#"><img className="card-img-top" src={Chompa} alt="articulo1"/></a>
+        <div className="card-body">
+            <h4 className="card-title">
+                <a className="article-tittle text-bold" href="#">Gradient T-shirt</a>
+            </h4>
+            <h5 className="subtitle">COP$ 60.000</h5>
+            <p className="card-text parrafo">Hermosa camiseta de color degradado azul-morado</p>
         </div>
+        <div className="card-footer">
+            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+        </div>
+    </div>
+</div>
+<div className="col-lg-4 col-md-6 mb-4">
+    <div className="card ">
+        <a href="#"><img className="card-img-top"  src={Chompa} alt="articulo2"/></a>
+        <div className="card-body">
+            <h4 className="card-title">
+                <a className="article-tittle text-bold" href="#">Gradient Hoddie</a>
+            </h4>
+            <h5 className="subtitle">COP$ 120.000</h5>
+            <p className="card-text parrafo">Increible hoddie con degradado de color morado-negro </p>
+        </div>
+        <div className="card-footer">
+            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+        </div>
+    </div>
+</div>
+<div className="col-lg-4 col-md-6 mb-4">
+    <div className="card ">
+        <a href="#"><img className="card-img-top" src={BlackTshirt} alt="articulo3"/></a>
+        <div className="card-body">
+            <h4 className="card-title">
+                <a className="article-tittle text-bold" href="#">T-shirt Nuevo Formato</a>
+            </h4>
+            <h5 className="subtitle">COP$ 40.000</h5>
+            <p className="card-text parrafo">Camiseta negra nuevo formato</p>
+        </div>
+        <div className="card-footer">
+            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+        </div>
+    </div>
+</div>
+<div className="col-lg-4 col-md-6 mb-4">
+    <div className="card">
+        <a href="#"><img className="card-img-top" src={WhiteTshirt} alt="articulo4"/></a>
+        <div className="card-body">
+            <h4 className="card-title">
+                <a className="article-tittle text-bold" href="#">T-shirt Nuevo Formato</a>
+            </h4>
+            <h5 className="subtitle">COP$ 40.000</h5>
+            <p className="card-text parrafo">Camiseta blanca nuevo formato</p>
+        </div>
+        <div className="card-footer">
+            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+        </div>
+    </div>
+</div>
+<div className="col-lg-4 col-md-6 mb-4">
+    <div className="card h-100">
+        <a href="#"><img className="card-img-top" src={Totebag} alt="articulo5"/></a>
+        <div className="card-body">
+            <h4 className="card-title">
+                <a className="article-tittle text-bold" href="#">Tote Bag Logo Cientonce</a>
+            </h4>
+            <h5 className="subtitle">COP$ 20.000</h5>
+            <p className="card-text parrafo">hermoso bolso ecológico para salir de compras y cuidar el medio ambiente</p>
+        </div>
+        <div className="card-footer">
+            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+        </div>
+    </div>
+</div>
+<div className="col-lg-4 col-md-6 mb-4">
+    <div className="card ">
+        <a href="#"><img className="card-img-top"  src={Gorra} alt="articulo6"/></a>
+        <div className="card-body">
+            <h4 className="card-title">
+                <a className="article-tittle text-bold" href="#">Gorra Logo Cientonce</a>
+            </h4>
+            <h5 className="subtitle">COP$ 25.000</h5>
+            <p className="card-text parrafo">gorra negra con costuras moradas tipo baceball</p>
+        </div>
+        <div className="card-footer">
+            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+        </div>
+    </div>
+</div>
+</div>
 
+
+
+
+
+        </div>
+        
 );
 
 };
