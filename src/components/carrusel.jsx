694aa81f3a1708dc22645cecb4f8f13b31924b4c
SharonOsorno <slosorno2@misena.edu.co>
2021-06-25 16:25:05 -0500

    commit
    flechas banner tienda

--- src/components/carrusel.jsx
+++ src/components/carrusel.jsx
@@ -30,13 +30,12 @@
             </div>
 
             <a className="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-            <span className="fas fa-arrow-circle-left flechas"aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-            </a>
-
-            <a className="carousel-control-next" href="#carousel" role="button" data-slide="next" >
-                <span className="fas fa-arrow-circle-right flechas" aria-hidden="true"></span>
-                <span className="sr-only" > Next </span>
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carousel" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
             </a>
         </div>
     </div>
